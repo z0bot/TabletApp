8a14ea8a9bbe1214e6c715a24faa49fd0ee5c053
quadfather42 <32621407+quadfather42@users.noreply.github.com>
2020-03-02 15:11:45 -0600

    Update menuPage.xaml.cs

--- TabletApp/TabletApp/Pages/menuPage.xaml.cs
+++ TabletApp/TabletApp/Pages/menuPage.xaml.cs
@@ -19,13 +19,28 @@ namespace TabletApp.Pages
 		{
 			InitializeComponent ();
 
+            //Entrees
             mpEntreeOne.Clicked += mpEntreeOne_Clicked;
             mpEntreeTwo.Clicked += mpEntreeTwo_Clicked;
+            mpEntreeThree.Clicked += mpEntreeThree_Clicked;
+            mpEntreeFour.Clicked += mpEntreeFour_Clicked;
 
+            //Drinks
+            mpDrinkOne.Clicked += mpDrinkOne_Clicked;
+            mpDrinkTwo.Clicked += mpDrinkTwo_Clicked;
+            mpDrinkThree.Clicked += mpDrinkThree_Clicked;
+
+            //Sides
+            mpSideOne.Clicked += mpSidesOne_Clicked;
+            mpSideTwo.Clicked += mpSidesTwo_Clicked;
+            mpSideThree.Clicked += mpSidesThree_Clicked;
+
+            //Bottom Buttons
             mpRefillButton.Clicked += mpRefillButton_Clicked;
             mpCallServerButton.Clicked += mpCallServerButton_Clicked;
 		}
 
+        //Entrees
         private async void mpEntreeOne_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new menuItemPage("STREET BUNG", "A lot of Meat")
@@ -39,6 +54,49 @@ namespace TabletApp.Pages
             await Navigation.PushAsync(new menuItemPage());
         }
 
+        private async void mpEntreeThree_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        private async void mpEntreeFour_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        //Drinks
+        private async void mpDrinkOne_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        private async void mpDrinkTwo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        private async void mpDrinkThree_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        //Sides
+        private async void mpSidesOne_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        private async void mpSidesTwo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        private async void mpSidesThree_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new menuItemPage());
+        }
+
+        //Bottom Buttons
         private async void mpRefillButton_Clicked(object sender, EventArgs e)
         {
             await DisplayAlert("Refill Request Sent", "Your Server is on their way", "Thristy Aint'cha");
